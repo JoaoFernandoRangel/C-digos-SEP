39c22012ca29bfdf892f8b5e1fcf9eb9c6d6689b
João Fernando Rangel <joaofrguimaraes1@gmail.com>
2024-09-29 16:38:15 -0300

    .

---
+++ P2/fasor.m
@@ -0,0 +1,4 @@
+function [Z] = fasor(modulo,angulo_deg)
+Z= modulo*exp(1i*deg2rad(angulo_deg));
+end
+
--- fasor.m
+++
@@ -1,4 +0,0 @@
-function [Z] = fasor(modulo,angulo)
-Z= modulo*exp(1i*deg2rad(angulo));
-end
-
